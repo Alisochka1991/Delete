fa942de403c7bfddbceccd08ff1c8d3f620054d9
Alisochka1991 <alisiaagranov@gmail.com>
2021-10-11 16:05:51 +0300

    comit hw

--- src/test/java/StartSelenium.java
+++ src/test/java/StartSelenium.java
@@ -1,9 +1,13 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class StartSelenium {
 
@@ -23,7 +27,29 @@ public class StartSelenium {
     @Test
     public void testName()
     {
+        WebElement element = wd.findElement(By.tagName("a"));
+        List<WebElement> elements = wd.findElements(By.tagName("a"));
+        System.out.println(elements.size());
 
+        wd.findElement(By.className("container"));
+        wd.findElement(By.className("navbar-component_nav__1X_4m"));
+        wd.findElement(By.className("login_login__3EHKB"));
+
+                wd.findElement(By.id("root"));
+
+                wd.findElement(By.tagName("a"));
+        wd.findElement(By.tagName("input"));
+        wd.findElement(By.tagName("button"));
+        wd.findElement(By.tagName("h"));
+
+                wd.findElement(By.linkText("HOME"));
+        wd.findElement(By.linkText("ABOUT"));
+        wd.findElement(By.linkText("LOGIN"));
+
+
+                wd.findElement(By.partialLinkText("HO"));
+        wd.findElement(By.partialLinkText("LOG"));
+        wd.findElement(By.partialLinkText("OUT"));
         //click login button
     }
     @Test public void testName2()
